fc99aee90ddca98edc6078de466ec14f518f7c20
Luciano <lucianoapatrindade@gmail.com>
2021-06-14 21:24:05 -0300

    Trabalhando com o JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,61 @@
 const codigo = document.querySelector('.codigo');
-const botaoCodigo = document.querySelector('.botao-codigo');
+const botaoConsultar = document.querySelector('.botao-consultar');
 const quantidade = document.querySelector('.quantidade');
 const descricao = document.querySelector('.produto');
-const valor = document.querySelector('.valor');
+const preco = document.querySelector('.valor');
 const desconto = document.querySelector('.desconto');
 const botaoCalcular = document.querySelector('.botao-calcular');
 const sub = document.querySelector('.subtotal');
 
 
-
+/*Feito um objetos com os produtos */
 const produto1 = {
     codigo: 123,
-    produto: 'Deo Parfum Far Away Royale',
+    produto: 'DEO PARFUM FAR AWAY ROYALE',
     valor: 42.90,
     produto2: {
         codigo: 456 ,
-        produto: 'Tinta Automotiva',
+        produto: 'KIT RITUAL RENEW',
         valor: 109.90,  
     },
     produto3: {
         codigo: 789,
-        produto: 'pincel para pintura',
-        valor: 22.90, 
-
+        produto: 'QUARTETO DE SOMBRAS METALICO MATTE 5G',
+        valor: 53.99, 
     }
 }
 
-
+/*Inclui os itens no campo de consulta e calculo */
 function incluiProduto(cod){
-    if(cod == 123){
+    const valor = parseInt(cod); // Estou convertendo String em Number
+    if(!valor) alert('O campo código aceita somente números'); // Inpedindo a entrada de //String.
+
+     if(valor == 123){
+         if(valor !== 123) alert('Produto não encontrado');
         descricao.innerText = produto1.produto;
-        valor.innerText = produto1.valor;
-        
+        preco.innerText = produto1.valor;
+    }
+    if(valor == 456){
+        descricao.innerText = produto1.produto2.produto;
+        preco.innerHTML = produto1.produto2.valor;
+    }
+    if(valor == 789){
+        descricao.innerText = produto1.produto3.produto;
+        preco.innerText = produto1.produto3.valor;
     }
 }
 
-function subTotal(cod1){
-    if(cod1 == 123){
-       const resultado = produto1.valor * quantidade.value * (desconto.value/100);
-       sub.innerHTML = resultado;
 
-    }
-
-}
-
-
-
-
-
-
-
-botaoCodigo.addEventListener('click', function (e){
+/*Estou pegando o evento do botão de consultar item */
+botaoConsultar.addEventListener('click', function (e){
     e.preventDefault();
+    console.log(codigo.value);
+    if(!codigo.value) return;
 
    incluiProduto(codigo.value);
-   subTotal(123);
-   
+   console.log(codigo.value)
+     
 });
 
 
-botaoCalcular.addEventListener('click', function (e){
-    e.preventDefault();
 
-  
-   subTotal(123);
-   
-});
