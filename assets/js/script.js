60998b4e835b948ee6be0493dc7505bc81d5cbad
Luciano <lucianoapatrindade@gmail.com>
2021-06-18 20:05:23 -0300

    Melhorando o código e comentário

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-let codigo = document.querySelector('.codigo');
-let botaoConsultar = document.querySelector('.botao-consultar');
+const codigo = document.querySelector('.codigo');
+const botaoConsultar = document.querySelector('.botao-consultar');
 const descricao = document.querySelector('.produto');
-let preco = document.querySelector('.valor')
-let quantidade = document.querySelector('.quantidade');
-let desconto = document.querySelector('.desconto');
-let botaoCalcular = document.querySelector('.botao-calcular');
+const preco = document.querySelector('.valor')
+const quantidade = document.querySelector('.quantidade');
+const desconto = document.querySelector('.desconto');
+const botaoCalcular = document.querySelector('.botao-calcular');
 const sub = document.querySelector('.subtotal');
 const incluir = document.querySelector('.botao-incluir');
 const tabelas = document.querySelector('.tabelas');
@@ -12,7 +12,7 @@ const img = document.querySelector('.fa-camera');
 
 
 
-/*Feito um objetos com os produtos */
+/*1- Criei um objeto produto que armazena código, produto e valor */
 const produto1 = {
     codigo: 123,
     produto: 'DEO PARFUM FAR AWAY ROYALE',
@@ -29,22 +29,15 @@ const produto1 = {
     }
 }
 
-/*Focando no campo de quantidade */
-function focaQuantidade() {
-    quantidade.focus();
-}
-
-/*Inclui os itens no campo de consulta e calculo */
+/*2-Após incluir o código e consultar essa função mostra o produto e o valor no campo de pesquisa */
 function incluiProduto(cod) {
     const valor = parseInt(cod); // Estou convertendo String em Number
-    if (!valor) alert('O campo código aceita somente números'); // Inpedindo a entrada de String.
+    if (!valor) alert('O campo código aceita somente números'); // Impede a entrada de String.
 
     if (valor == 123) {
         descricao.innerText = produto1.produto;
-
         preco.innerText = produto1.valor;
         incluirItem(produto1.produto);
-        //incluirItem(produto1.valor);
         quantidade.value = '';
         focaQuantidade();
     } else if (valor == 456) {
@@ -66,14 +59,16 @@ function incluiProduto(cod) {
     }
 }
 
-/*Função que faz o calculo do subtotal do produto*/
+/*3-Essa função após consultar o produto ela deixa o ponteiro do mouse no campo de quantidade, Assim mostra ao usuário qual é o próximo campo a ser preenchido */
+function focaQuantidade() {
+    quantidade.focus();
+}
 
+/*4-Apos imncluir a quantidade e o desconto, essa função faz o calculo e imprime o valor do resultado no campo SUBTOTAL*/
 function calculaSubtotal(valor, desc, qtda) {
     const preco = valor;
     const quant = qtda;
     const desconto = (desc / 100);
-    let tot = [];
-    console.log(tot);
 
     const calculaDesconto = preco - (preco * desconto);
     const total = parseInt(calculaDesconto * quant);
@@ -81,41 +76,56 @@ function calculaSubtotal(valor, desc, qtda) {
     incluirItem(calculaDesconto.toFixed(2));
     incluirItem(parseInt(quantidade.value));
     incluirItem(total.toFixed(2));
-    
+
     sub.innerHTML = total.toFixed(2);
 
-   return total.toFixed(2);
-    
+    return total.toFixed(2);
 }
 
-/*Recebe os dados do formulário para depois incluir no campo de orçamento */
+/*4-Esta função faz uma consulta no código e mostra a imagem do produto no campo de imagem */
+function mostraImagem(cod) {
+    const imagem1 = document.querySelector('.img-1');
+    const imagem2 = document.querySelector('.img-2');
+    const imagem3 = document.querySelector('.img-3');
+
+    const codigo = parseInt(cod);
+
+    codigo === 123 ? imagem1.style.display = "inline-block" : imagem1.style.display = "none"; img.style.display = "inline-block";
+    codigo === 456 ? imagem2.style.display = "inline-block" : imagem2.style.display = "none";
+    codigo === 789 ? imagem3.style.display = "inline-block" : imagem3.style.display = "none";
+
+    img.style.display = "none";
+}
+
+/*5-Este array recebe os dados para inclução no campo do orçameto*/
 let dados = [];
 
-/*Limpa a lista de array existente para nova inclusão ============================*/
-function limpaArray(){
+/*6- Esta função recebe um valor e faz a inclusão do mesmo no Array */
+function incluirItem(cod) {
+    dados.push(cod);
+}
+
+/*6- Limpa a lista de array existente para nova inclusão ============================*/
+function limpaArray() {
     for (let i = 0; i < dados.length; i++) {
         dados.splice(i);
     }
 }
 
-function incluirItem(cod) {
-    dados.push(cod);
-}
-
-/*função que cria TR */
+/*7- função que cria a tabela <tr> */
 function criaTr() {
     const tr = document.createElement('tr');
     return tr;
 }
 
-/*Função que cria tabela */
+/*8- Função que cria tabela <td>*/
 function criaTabela() {
     const td = document.createElement('td');
     return td;
 }
 
 
-/*Função que vai criar a tabela no orçamento */
+/*8- Esta função vai pegar a informação que esta no array dados e vai coloca-los em forma de tabela na parte do orçamento.*/
 
 function tabelaOrcamento() {
     const td = criaTr();
@@ -123,45 +133,22 @@ function tabelaOrcamento() {
     for (let i = 0; i < dados.length; i++) {
         const tab = criaTabela();
         tab.innerHTML = dados[i];
-        tab.setAttribute('class', dados[i].length);//corrigir
-        
         td.appendChild(tab);
     }
 }
 
-/* Limpa o formulário após a inclusão do item na tabela de orçamento */
-function limpaFormulario(){
+/*9- Limpa o formulário após a inclusão do item na tabela de orçamento */
+function limpaFormulario() {
     codigo.value = '';
     codigo.focus();
     descricao.innerHTML = '';
     preco.innerHTML = '';
     quantidade.value = '';
-    desconto.value = ' ';
-    sub.innerHTML = 0;
-
+    desconto.value = '';
+    sub.innerHTML = '';
 }
 
-/* mostra imagem de acordo como o código digitado */
-
-function mostraImagem(cod){
-    const imagem1 = document.querySelector('.img-1');
-    const imagem2 = document.querySelector('.img-2');
-    const imagem3 = document.querySelector('.img-3');
-    
-    const codigo = parseInt(cod);
-
-    codigo === 123 ? imagem1.style.display = "inline-block" : imagem1.style.display = "none"; img.style.display = "inline-block" ;
-    codigo === 456 ? imagem2.style.display = "inline-block" : imagem2.style.display = "none";
-    codigo === 789 ? imagem3.style.display = "inline-block" : imagem3.style.display = "none" ;
-
-     img.style.display = "none";
-}
-
-/*Incluindo o valor total dos itens na tabela do orçamento */
-
-
-
-/*Estou pegando o evento do botão de consultar item */
+/*10- Estou pegando o evento do botão de consultar item */
 botaoConsultar.addEventListener('click', function (e) {
     e.preventDefault();
     if (!codigo.value) return;
@@ -172,39 +159,35 @@ botaoConsultar.addEventListener('click', function (e) {
 
 });
 
-/*Pegando o evento do botão calcular */
-
+/*11- Pegando o evento do botão calcular */
 botaoCalcular.addEventListener('click', function (e) {
     e.preventDefault();
-    if (!quantidade.value || !desconto.value) alert("Prencher quantidade e desconto!");
+    if (!quantidade.value || !desconto.value) return alert("Prencher quantidade e desconto!") ;
 
     calculaSubtotal(preco.value, desconto.value, quantidade.value);
     //incluirItem(parseInt(quantidade.value));
 });
 
-/*Pegando o evento do botão incluir */
-
+/*12- Pegando o evento do botão incluir */
 incluir.addEventListener('click', function (e) {
     e.preventDefault();
 
     tabelaOrcamento();
-    limpaArray(); 
+    limpaArray();
     limpaFormulario();
     mostraImagem();
-     img.style.display = "flex";
+    img.style.display = "flex";
 });
 
-
-
-/*Parando o evento da tecla enter e quando precionada foca no campo desconto.*/
-quantidade.addEventListener('keypress', function(e){
-    if(e.keyCode === 13) e.preventDefault();
+/*13- Parando o evento da tecla enter e quando precionada foca no campo desconto.*/
+quantidade.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) e.preventDefault();
     desconto.focus();
 });
 
-/* Estou parando o evendo do enter no campo desconto */
-desconto.addEventListener('keypress', function(e){
-    if(e.keyCode === 13) e.preventDefault();
+/*14- Estou parando o evendo do enter no campo desconto */
+desconto.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) e.preventDefault();
 });
 
 
